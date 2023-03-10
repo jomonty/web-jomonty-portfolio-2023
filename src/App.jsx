aa25ef2b8fe696712b836617bd1a0ba0c0b42e55
jomonty <jomonty_cs@outlook.com>
2023-03-10 21:49:08 +0000

    scaffold

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 class="text-emerald-400 animate-pulse flex justify-center p-10">Arrr, it's a header.</h1>
+    </div>
+  )
+}
+
+export default App
