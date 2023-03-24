7c5bbb4dc26ecb415b72d0c2b7cca0889452dcdb
jomonty <jomonty_cs@outlook.com>
2023-03-24 19:41:09 +0000

    removed grid format for projects and tech

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -5,16 +5,14 @@ const Projects = ({ navOpen }) => {
         <div className="mx-auto max-w-7xl px-4 lg:px-8">
           <div className="relative sm:px-8 lg:px-12">
             <div className="mx-auto max-w-2xl lg:max-w-5xl">
-              <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-                <div className="">
-                  <h1 className="text-3xl font-extrabold sm:text-4xl md:text-5xl">
-                    Stuff I've made in my journey so far.
-                  </h1>
-                  <p className="pt-8 text-base tracking-wider">
-                    Coming soon...
-                  </p>
-                </div>
+              {/* <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12"> */}
+              <div className="">
+                <h1 className="text-3xl font-extrabold sm:text-4xl md:text-5xl">
+                  Stuff I've made in my journey so far.
+                </h1>
+                <p className="pt-8 text-base tracking-wider">Coming soon...</p>
               </div>
+              {/* </div> */}
             </div>
           </div>
         </div>
--- src/containers/Tech.jsx
+++ src/containers/Tech.jsx
@@ -5,16 +5,14 @@ const Tech = ({ navOpen }) => {
         <div className="mx-auto max-w-7xl px-4 lg:px-8">
           <div className="relative sm:px-8 lg:px-12">
             <div className="mx-auto max-w-2xl lg:max-w-5xl">
-              <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-                <div className="">
-                  <h1 className="text-3xl font-extrabold sm:text-4xl md:text-5xl">
-                    Hardware and software I use day-to-day.
-                  </h1>
-                  <p className="pt-8 text-base tracking-wider">
-                    Coming soon...
-                  </p>
-                </div>
+              {/* <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12"> */}
+              <div className="">
+                <h1 className="text-3xl font-extrabold sm:text-4xl md:text-5xl">
+                  Hardware and software I use day-to-day.
+                </h1>
+                <p className="pt-8 text-base tracking-wider">Coming soon...</p>
               </div>
+              {/* </div> */}
             </div>
           </div>
         </div>
