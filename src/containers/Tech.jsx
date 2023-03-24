284252851415d768ee2ec013e22bd3d290b20f49
jomonty <jomonty_cs@outlook.com>
2023-03-24 19:39:10 +0000

    add template structure to projects and tech

--- src/containers/Tech.jsx
+++ src/containers/Tech.jsx
@@ -1,11 +1,23 @@
 const Tech = ({ navOpen }) => {
   return (
     <main>
-      <div className="container relative mx-auto w-[85vw] sm:px-8 lg:px-12">
-        <h1 className="min-w-sm text-3xl font-extrabold sm:text-5xl md:w-[40vw] md:min-w-md">
-          Hardware and software I use day-to-day.
-        </h1>
-        <p className="flex justify-center p-10">Tech</p>
+      <div className="mt-6 sm:px-8">
+        <div className="mx-auto max-w-7xl px-4 lg:px-8">
+          <div className="relative sm:px-8 lg:px-12">
+            <div className="mx-auto max-w-2xl lg:max-w-5xl">
+              <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+                <div className="">
+                  <h1 className="text-3xl font-extrabold sm:text-4xl md:text-5xl">
+                    Hardware and software I use day-to-day.
+                  </h1>
+                  <p className="pt-8 text-base tracking-wider">
+                    Coming soon...
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
