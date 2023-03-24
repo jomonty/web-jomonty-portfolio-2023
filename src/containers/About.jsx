06cb0ff744a7bb226dbb638ce80796d4d5a79ce7
jomonty <jomonty_cs@outlook.com>
2023-03-24 14:17:18 +0000

    added headers to main pages

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,7 +1,12 @@
 const About = ({ navOpen }) => {
   return (
     <main>
-      <p className="flex justify-center p-10">About</p>
+      <div className="container relative mx-auto w-[85vw] sm:px-8 lg:px-12">
+        <h1 className="min-w-sm text-3xl font-extrabold sm:text-5xl md:w-[40vw] md:min-w-md">
+          I'm Josh Montgomery, full-stack software developer in Glasgow.
+        </h1>
+        <p className="flex justify-center p-10">About</p>
+      </div>
     </main>
   );
 };
