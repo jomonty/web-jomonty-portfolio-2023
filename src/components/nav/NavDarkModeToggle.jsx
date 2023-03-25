df685083eaadfb34f958c1caf55623de9424c504
jomonty <jomonty_cs@outlook.com>
2023-03-25 23:09:02 +0000

    remove div from dark mode toggle

--- src/components/nav/NavDarkModeToggle.jsx
+++ src/components/nav/NavDarkModeToggle.jsx
@@ -2,11 +2,12 @@ import { Sun, Moon } from "react-feather";
 
 const NavDarkModeToggle = ({ darkMode, toggleTheme }) => {
   return (
-    <div className="flex rounded-full shadow dark:bg-zinc-800 dark:ring-1 dark:ring-white/10">
-      <button className="py-2 px-3" onClick={() => toggleTheme()}>
-        {darkMode ? <Moon color="#71717a" /> : <Sun color="rgb(21 128 61)" />}
-      </button>
-    </div>
+    <button
+      className="flex rounded-full py-2 px-3 shadow dark:bg-zinc-800 dark:ring-1 dark:ring-white/10"
+      onClick={() => toggleTheme()}
+    >
+      {darkMode ? <Moon color="#71717a" /> : <Sun color="rgb(21 128 61)" />}
+    </button>
   );
 };
 
