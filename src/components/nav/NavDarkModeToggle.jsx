96f6b47d9d2219e8de0947cd3f9563852a3530f9
jomonty <jomonty_cs@outlook.com>
2023-03-25 23:06:42 +0000

    fix dark mode toggle button

--- src/components/nav/NavDarkModeToggle.jsx
+++ src/components/nav/NavDarkModeToggle.jsx
@@ -2,8 +2,8 @@ import { Sun, Moon } from "react-feather";
 
 const NavDarkModeToggle = ({ darkMode, toggleTheme }) => {
   return (
-    <div className="flex rounded-full py-2 px-3 shadow dark:bg-zinc-800 dark:ring-1 dark:ring-white/10">
-      <button onClick={() => toggleTheme()}>
+    <div className="flex rounded-full shadow dark:bg-zinc-800 dark:ring-1 dark:ring-white/10">
+      <button className="py-2 px-3" onClick={() => toggleTheme()}>
         {darkMode ? <Moon color="#71717a" /> : <Sun color="rgb(21 128 61)" />}
       </button>
     </div>
