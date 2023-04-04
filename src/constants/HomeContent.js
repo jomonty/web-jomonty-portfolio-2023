7f7df86ab467ec4af5d1025237ae27ceb88e4f37
jomonty <jomonty_cs@outlook.com>
2023-04-04 14:21:10 +0100

    update home page content

--- src/constants/HomeContent.js
+++ src/constants/HomeContent.js
@@ -1,7 +1,7 @@
 const HomeContent = {
   title: "Software developer, analyst, lifelong learner.",
   text: [
-    "I'm Josh, a full-stack developer based in Glasgow. Experienced analyst, curious by nature, committed to cool tech.",
+    "I'm Josh, a full-stack developer based in Glasgow. Experienced analyst, curious by nature, outdoor enthusiast, fascinated by cool tech.",
   ],
 };
 
