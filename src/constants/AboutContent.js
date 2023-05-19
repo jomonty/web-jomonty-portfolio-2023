6329b60e2e6c9833488a124bffe8bf0265c002f6
jomonty <jomonty_cs@outlook.com>
2023-05-19 19:40:46 +0100

    updates to about

--- src/constants/AboutContent.js
+++ src/constants/AboutContent.js
@@ -1,13 +1,19 @@
 export const AboutContent = {
   title:
-    "I'm Josh Montgomery. A full-stack software developer based in Glasgow.",
+    "I'm Josh Montgomery. A full-stack software developer based in Scotland.",
   text: [
     "I've always been a tinkerer, completely facinated by how things work. Academically strong in maths, I opted to study it at university. After I completed my degree I got a job in energy trading and pricing, where I spent 5 years.",
     "While working in utilities, I spent a lot of time using databases, primarily relational, and tools such as SSMS, VBA and Python to interact with them. Eventually I realised that using these tools was building a desire to understand how software works.",
     "I began to teach myself through self-guided study, online courses and building some personal projects, the more time I spent studying the more I became convinced that this is where I should be working. So, in 2022, I decided to pursue software development as a career and enrolled at CodeClan Glasgow for a 4-month course in professional software development. It was an incredible experience, it built my knowledge, confidence and I massively enjoyed it.",
-    "I'm now actively looking for employment, if you'd like to get in touch please drop me a message on LinkedIn, I'm always available for a chat. Until then, I will keep tinkering and building my knowledge!",
+    "I'm now working as a Software Engineer at Ivanti, continuing my learning both on and off the job.",
   ],
   workHistory: [
+    {
+      company: "Ivanti",
+      role: "Associate Software Engineer",
+      dates: "May 2023 - Present",
+      logo: "",
+    },
     {
       company: "CodeClan",
       role: "Junior Developer",
@@ -34,5 +40,3 @@ export const AboutContent = {
     },
   ],
 };
-
-// export default AboutContent;
--- src/constants/HomeContent.js
+++ src/constants/HomeContent.js
@@ -1,7 +1,7 @@
 const HomeContent = {
   title: "Software developer, analyst, lifelong learner.",
   text: [
-    "I'm Josh, a full-stack developer based in Glasgow. Experienced analyst, curious by nature, outdoor enthusiast, fascinated by cool tech.",
+    "I'm Josh, a full-stack developer based in Scotland. Experienced analyst, curious by nature, outdoor enthusiast, fascinated by cool tech.",
   ],
 };
 
